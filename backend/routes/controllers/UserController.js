e8d49d2983b7b83b813c3dcb26acbc31e25599eb
Sophia Thomas <sophiart03@gmail.com>
2025-09-18 00:21:12 -0700

    refactor: user router

--- backend/routes/controllers/UserController.js
+++ backend/routes/controllers/UserController.js
@@ -3,8 +3,61 @@ import User from '../../model/user.model.js'
 export const getAllUsers = async (req, res) => {
     try {
         const users = await User.find()
+        console.log(req.query.name)
         res.status(200).json(users)
     } catch(error) {
         res.status(500).json({message: error.message});
     }
 };
+
+export const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id)
+        if (user) {
+            res.status(200).json(user)
+        } else {
+            return res.status(404).json({message: "User not found"})
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const postUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body); 
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+export const putUserById = async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const user = await User.findByIdAndUpdate(id, req.body, {new: true, runValidators: true}); 
+        if (!user) {
+            return res.status(404).json({message: "User not found"}); 
+        }
+        const updatedUser = await User.findById(id); 
+        res.status(200).json(user)
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const deleteUserById = async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const deletedDoc = await User.findByIdAndDelete(id)
+        if (deletedDoc) {
+            console.log('User document deleted:', deletedDoc);
+        } else {
+            console.log('Document not found.');
+            return res.status(404).json({message: "User not found"})
+        }
+    } catch(error) {
+        res.status(500).json({message: error.message})
+    }
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,43 +1,27 @@
-import { getAllUsers } from './controllers/UserController.js'
-import User from '../model/user.model.js'
+import { 
+  getAllUsers, 
+  getUserById, 
+  postUser,
+  putUserById,
+  deleteUserById 
+} from './controllers/UserController.js'
 import express from "express";
 
 const router = express.Router()
 
-// Get all Users 
-router.get('/', getAllUsers)
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(postUser)
 
-// Add a new user 
-router.post('/', async (req, res) => {
-  try {
-    const user = await User.create(req.body); 
-    res.status(200).json(user)
-  } catch (error) {
-    res.status(500).json({message: error.message})
-  }
-})
-
-// Get new users
 router.get('/new', (req, res) => {
   res.send("User New Form")
 })
 
-// router.route('/:name').get((req, res) => {
-//     req.params.name
-//     res.send(`Get User with ID ${req.params.body}`)
-// }).put((req, res) => {
-//     req.params.name
-//     res.send(`Put User with ID ${req.params.name}`)
-// }).delete((req, res) => {
-//     res.send(`Delete ${res.params.name}`)
-// })
-
-// // Dynamic parameter /:{}
-// router.get((req, res) => {
-//     req.params.name
-//     res.send(`Get User with ID ${req.params.name}`)})
-// router.put('/:name', (req, res) => {
-//     req.params.name
-//     res.send(`Get User with ID ${req.params.name}`)})
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(putUserById)
+  .delete(deleteUserById)
 
 export default router
