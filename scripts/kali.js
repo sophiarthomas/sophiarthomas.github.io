f2ce2b0ff82ef4b3869845dbe77fee78b49badc8
Sophia Thomas <sophiart03@gmail.com>
2026-01-31 16:20:18 -0800

    Makefile deploy at Sat Jan 31 16:20:18 PST 2026

--- scripts/kali.js
+++ scripts/kali.js
@@ -1,25 +1,30 @@
 const windowEl = document.getElementById("window"); 
 const fileList = document.getElementById("fileList"); 
 const folder = document.querySelector(".folder");
+const cloud = '../assets/img/icons/cloud.png'
 
 
 /* projects */
 const projects = [
-     { name: "EXPLORE", icon: "" },
-     { name: "Kali Linux Emulator", icon: "" }
+     { name: "EXPLORE", icon: cloud },
+     { name: "Kali Linux Emulator", icon: cloud },
+     { name: "Digital Certificate Automation", icon: cloud}
 ];
 
 /* Open window */
 function openWindow() {
-     // console.log(windowEl.outerHTML);
      windowEl.classList.remove("hidden");
 
      fileList.innerHTML = projects
-     .map(p => `<div class="file">${p.icon} ${p.name}</div>`)
-    .join("");
+     .map(p => 
+          `<div class="file">
+           <img src="${p.icon}" class="file-icon" />
+          <span>${p.name}</span>
+          </div>`)
+    .join(" ");
 }
 
 function closeWindow() {
      windowEl.classList.add("hidden"); 
 }
-folder.addEventListener("click", openWindow);
+folder.addEventListener("dblclick", openWindow);
